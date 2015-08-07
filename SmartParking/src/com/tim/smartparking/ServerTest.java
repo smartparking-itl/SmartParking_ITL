91c9b0759510991b59ab4ba0bbf6b08d29daa8a4
Malik1998 <gazizullin.malik2010@yandex.ru>
2015-08-07 16:00:40 +0300

    кароч я закалибался не знаю как через консоль скидывать файлы. в инете не нашел. аааааааааааааааа пох.

--- SmartParking/src/com/tim/smartparking/ServerTest.java
+++ SmartParking/src/com/tim/smartparking/ServerTest.java
@@ -1,5 +1,13 @@
 package com.tim.smartparking;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
@@ -7,12 +15,15 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -24,6 +35,9 @@ public class ServerTest extends Activity {
 	private static ImageView[] ivCar = new ImageView[count_of_cars];
 	public static String LOG_TAG = "my_log";
 	public static String s = "";
+	private int sch = 0;
+	private double TimeIn = 0;
+	
 
 
 	  private static void setColorCars(String s) {
@@ -55,9 +69,94 @@ public class ServerTest extends Activity {
         };*/
 		ivCar[0] = (ImageView) findViewById(R.id.imageView2);
 		ivCar[1] = (ImageView) findViewById(R.id.ImageView3);
-		//Toast.makeText(getApplicationContext(), "Created", Toast.LENGTH_SHORT).show();
-		//ivCar[0].setOnClickListener(ocl);
-		//ivCar[1].setOnClickListener(ocl);
+		
+		ivCar[0].setOnClickListener(new OnClickListener() {
+			
+			private int i = 0;
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				if(sch == 0)
+				{
+					sch = 1;
+					TimeIn = System.currentTimeMillis();
+				}
+				
+				double TimeOut = System.currentTimeMillis();
+				
+				if((TimeOut-TimeIn)*0.001<=15) sch++;
+				else sch = 0;
+				
+				if(sch==8)
+				{
+					AlertDialog.Builder dialog = new AlertDialog.Builder(ServerTest.this);
+					final EditText txt = new EditText(ServerTest.this);
+					txt.setVisibility(View.VISIBLE);
+					dialog.setView(txt);
+					
+					dialog.setCancelable(true);
+					dialog.setPositiveButton("Set", new DialogInterface.OnClickListener(){
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							// TODO Auto-generated method stub
+							
+				        	String text = String.valueOf(txt.getText());
+				        	int nom = 0;
+				        	int val = 0;
+				        	for(int i = 0; i<text.length(); i++)
+				        	{
+				        		if(text.charAt(i)!='=')
+				        		{
+				        			nom = nom * 10 + (int)(text.charAt(i)-'0');
+				        		}
+				        		else break;
+				        	}
+				        	
+				        	if(text.charAt(text.length()-1)=='1')
+				        	{
+				        		val = 1;
+				        	}
+				        	
+				            GettingInfo gf = new GettingInfo(ServerTest.this);
+				            try {
+								try {
+									String inf = gf.execute(web_site + "?nom=" + nom + "&val=" +  val).get(3000, TimeUnit.MILLISECONDS);
+								} catch (TimeoutException e) {
+									// TODO Auto-generated catch block
+									Toast.makeText(ServerTest.this, "Error in time out", Toast.LENGTH_SHORT).show();
+								}
+							} catch (InterruptedException e) {
+								Toast.makeText(ServerTest.this, "Error connection", Toast.LENGTH_SHORT).show();
+								
+							} catch (ExecutionException e) {
+								// TODO Auto-generated catch block
+								Toast.makeText(ServerTest.this, "Error in connection", Toast.LENGTH_SHORT).show();
+							}
+				            
+				            
+				        dialog.dismiss();
+				        get_place();
+							
+						}
+					});
+					
+					dialog.setNegativeButton("Close", new DialogInterface.OnClickListener(){
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.dismiss();
+						}
+					});
+					
+					dialog.show();
+					sch = 0;
+				}
+				
+			}
+		});;
+		
+    
 
         get_place();
         
@@ -86,13 +185,15 @@ public class ServerTest extends Activity {
 				Toast.makeText(getApplicationContext(), "Error getting info", Toast.LENGTH_SHORT).show();
 				return;
 			}
-			Log.e("Error", "eroric");
+			
+			//Toast.makeText(getApplicationContext(), ginfo, Toast.LENGTH_LONG).show();
+			//ginfo. = 'g';
+		//	ginfo = ginfo.substring(0, 1) +'o' + ginfo.substring(3);
+			
 			JSONWorking jw = new JSONWorking(getApplicationContext());
 	         
 	        String scolor = "";
-	         Log.e("er", "tut");
 	         try {
-	        	 Log.e("er", "perhaps here");
 					ArrayList<HashMap<String, String>>  res = jw.execute(ginfo).get();
 					
 				//	Log.e("json","done");
